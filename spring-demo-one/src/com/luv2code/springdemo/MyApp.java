7dd8d3ee5c96df9ed00cedf0b8184b6766a55aa9
csseifms@gmail.com <csseifms@gmail.com>
2021-12-17 15:01:24 +0200

    componentScan annotation

--- spring-demo-one/src/com/luv2code/springdemo/MyApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/MyApp.java
@@ -1,13 +1,16 @@
 package com.luv2code.springdemo;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+@ComponentScan("com.luv2code.springdemo")
 public class MyApp {
 
 	public static void main(String[] args) {
 		// load the spring configuration file
-		ClassPathXmlApplicationContext context = 
-				new ClassPathXmlApplicationContext("applicationContext.xml");
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(MyApp.class);
 				
 		// retrieve bean from spring container
 		Coach theCoach = context.getBean("trackCoach", Coach.class);
